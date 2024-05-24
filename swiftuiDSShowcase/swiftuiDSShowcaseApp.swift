ece5add181d1251ca0d5788a3c55d24bf37f6942
Giovanni Martusciello <giovanni.martusciello@gmail.com>
2024-05-24 18:09:55 +0200

    Initial Commit

---
+++ swiftuiDSShowcase/swiftuiDSShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiDSShowcaseApp.swift
+//  swiftuiDSShowcase
+//
+//  Created by Giovanni Martusciello on 24.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiDSShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
