68f9a6075c21476ba0f61e3f0eaeaf8262fba73b
giovannimartusciello <giovannimartusciello@gmail.com>
2024-05-24 17:55:42 +0000

    Update from https://github.com/giovannimartusciello/style-dictionary-automator/commit/3fccde1b659c5a5955f8ede7be4fba920866e077

---
+++ Design\ System/BorderRadius.swift
@@ -0,0 +1,22 @@
+
+//
+// BorderRadius.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 24 May 2024 17:55:41 GMT
+
+
+import SwiftUI
+
+enum BorderRadius {
+    static let base = CGFloat(4.00)
+    static let full = CGFloat(9999.00)
+    static let lg = CGFloat(8.00)
+    static let md = CGFloat(6.00)
+    static let none = CGFloat(1.00)
+    static let sm = CGFloat(2.00)
+    static let xl = CGFloat(12.00)
+    static let xxl = CGFloat(16.00)
+    static let xxxl = CGFloat(24.00)
+}
---
+++ Design\ System/BorderWidth.swift
@@ -0,0 +1,18 @@
+
+//
+// BorderWidth.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 24 May 2024 17:55:41 GMT
+
+
+import SwiftUI
+
+enum BorderWidth {
+    static let lg = CGFloat(4.00)
+    static let md = CGFloat(2.00)
+    static let none = CGFloat(0.00)
+    static let sm = CGFloat(1.00)
+    static let xl = CGFloat(8.00)
+}
