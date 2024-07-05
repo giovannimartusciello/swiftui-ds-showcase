273bd2384c1e348adf03ae6eb405ad843adcdd37
Giovanni Martusciello <giovanni.martusciello@gmail.com>
2024-07-05 15:46:26 +0000

    Update from https://github.com/ricardo-ch/ds-style-automator/commit/ecdd8afd734650276f553c7b1908c7238d6cec04

---
+++ Design System/Size.swift
@@ -0,0 +1,23 @@
+
+//
+// Size.swift
+//
+
+// Do not edit directly
+// Generated on Fri, 05 Jul 2024 15:46:25 GMT
+
+
+import SwiftUI
+
+enum Size {
+    static let _0 = CGFloat(0.00)
+    static let _1 = CGFloat(1.00)
+    static let _16 = CGFloat(16.00)
+    static let _24 = CGFloat(24.00)
+    static let _32 = CGFloat(32.00)
+    static let _4 = CGFloat(4.00)
+    static let _40 = CGFloat(40.00)
+    static let _48 = CGFloat(48.00)
+    static let _64 = CGFloat(64.00)
+    static let _8 = CGFloat(8.00)
+}
